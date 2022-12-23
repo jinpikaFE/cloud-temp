1109673235dc1e1676ba89f7e420e9374954a1f0
jinxin <jinxin@hsmap.com>
2022-12-23 16:38:13 +0800

    chore: 代码生成修改

---
+++ 新建文件夹/temp-generator/src/test/java/com/jinpika/temp/generator/TempGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinpika.temp.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
