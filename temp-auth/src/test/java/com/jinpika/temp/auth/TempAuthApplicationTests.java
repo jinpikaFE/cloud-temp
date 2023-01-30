8bf08606542af8af62b38904e534739932354e1a
jinxin <jinxin@hsmap.com>
2023-01-30 15:18:25 +0800

    feat: auth结构添加

---
+++ temp-auth/src/test/java/com/jinpika/temp/auth/TempAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinpika.temp.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
